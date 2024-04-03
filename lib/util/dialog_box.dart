c335714b916df382212a819f20172ba53fd08006
Esther Njihia <esthernjihia44@gmail.com>
2024-04-03 06:08:11 -0700

    Added save/cancel buttons

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
+import 'package:todo_app/util/my_button.dart';
 
 class DialogBox extends StatelessWidget {
   const DialogBox({super.key});
@@ -10,7 +11,7 @@ class DialogBox extends StatelessWidget {
     return AlertDialog(
       backgroundColor: Color.fromARGB(255, 133, 196, 247),
       content: Container(
-        height: 120,
+        height: 180,
         child: Column(
           children: [
             //get user input
@@ -22,6 +23,10 @@ class DialogBox extends StatelessWidget {
             ),
 
             //buttons -> save + cancel
+            MyButton(text: "Save", onPressed: () {}),
+
+            //cancel button
+            MyButton(text: "Cancel", onPressed: () {})
           ],
         ),
       ),
